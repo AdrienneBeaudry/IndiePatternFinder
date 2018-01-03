6c9870461b7f86bb6d7e3194e3113b5027b9d6d0
AdrienneBeaudry <adrienne.beaudry@gmail.com>
2018-01-03 14:26:05 +0100

    Progress on figuring out QueryPath selectors for each piece of data

--- app/Console/Commands/GetNamedPage.php
+++ app/Console/Commands/GetNamedPage.php
@@ -39,8 +39,43 @@ class GetNamedPage extends Command
     public function handle()
     {
 
-        print QueryPath::withHTML('https://www.namedclothing.com/product-category/all-patterns/page/2/', 'title')->text();
+
+        /*
+         * NAMES OK
+        // getting all product name in an array
+        print_r(QueryPath::withHTML('https://www.namedclothing.com/product-category/all-patterns/page/2/', '.product h3')->toArray());
         die();
+        */
+
+        /*
+         * PRICES OK
+         * Storing all prices in an array
+        print_r(QueryPath::withHTML('https://www.namedclothing.com/product-category/all-patterns/page/2/', '.product .price')->toArray());
+        die();
+        */
+
+
+        var_dump(QueryPath::withHTML('https://www.namedclothing.com/product-category/all-patterns/page/2/')->find('.product a')->attr());
+        die();
+
+        /*
+         * Product page URL
+        print QueryPath::withHTML('https://www.namedclothing.com/product-category/all-patterns/page/2/', '.product a')->attr('href');
+        die();
+        */
+
+        /*
+        First product picture URL, but only for the first child... don't know why
+        print QueryPath::withHTML('https://www.namedclothing.com/product-category/all-patterns/page/2/', '.product a img')->attr('src');
+        die();
+        */
+
+        /* Creates a long string joining all text from product on the page corresponding to the path
+        print QueryPath::withHTML('https://www.namedclothing.com/product-category/all-patterns/page/2/', '.product')->text();
+        ///ALSO the below will select only the h3
+        print QueryPath::withHTML('https://www.namedclothing.com/product-category/all-patterns/page/2/', '.product h3')->text();
+        die();
+         */
 
         $ch = curl_init();
         $url = "https://www.namedclothing.com/product-category/all-patterns/page/2/";
