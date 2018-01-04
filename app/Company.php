aab72ca11729757660ae9da7fa68afb4b5c65128
AdrienneBeaudry <adrienne.beaudry@gmail.com>
2018-01-04 10:07:53 +0100

    Fill out model, add table relations

--- app/Company.php
+++ app/Company.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    public $incrementing = true;
+    public $timestamps = true;
+
+    protected $fillable = [
+        'id',
+        'name',
+    ];
+
+    /**
+     * @return mixed
+     */
+    public function pattern() {
+        return $this->hasMany(Pattern::class);
+    }
 }
--- app/Pattern.php
+++ app/Pattern.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pattern extends Model
 {
+    public $incrementing = true;
+    public $timestamps = true;
 
+    protected $fillable = [
+        'id',
+        'name',
+        'company_id',
+        'redirect_url',
+        'price',
+        'size_type',
+        'format',
+        'category',
+        'image_url',
+        'description',
+    ];
+
+    public function company() {
+        return $this->belongsTo(Company::class);
+    }
 }
