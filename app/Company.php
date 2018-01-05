1770a609df299169ed6c2a585d305036383efc3f
AdrienneBeaudry <adrienne.beaudry@gmail.com>
2018-01-05 14:57:36 +0100

    update ide helpers

--- app/Company.php
+++ app/Company.php
@@ -9,6 +9,14 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Pattern[] $pattern
  * @mixin \Eloquent
+ * @property int $id
+ * @property string $name
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company whereUpdatedAt($value)
  */
 class Company extends Model
 {
--- app/Pattern.php
+++ app/Pattern.php
@@ -9,6 +9,38 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @property-read \App\Company $company
  * @mixin \Eloquent
+ * @property int $id
+ * @property string $name
+ * @property string $company_id
+ * @property string $redirect_url
+ * @property string|null $company_product_id
+ * @property string|null $price
+ * @property string|null $size_type
+ * @property string|null $format
+ * @property string|null $category
+ * @property string|null $image_url
+ * @property string|null $description
+ * @property string|null $supplies
+ * @property string|null $language
+ * @property string|null $full_description
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern whereCategory($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern whereCompanyId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern whereCompanyProductId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern whereFormat($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern whereFullDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern whereImageUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern whereLanguage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern whereRedirectUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern whereSizeType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern whereSupplies($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pattern whereUpdatedAt($value)
  */
 class Pattern extends Model
 {
