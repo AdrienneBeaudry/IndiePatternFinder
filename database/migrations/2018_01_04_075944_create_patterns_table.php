64fa15a1e41ef08fe36b48c52a89c6eff5596d69
AdrienneBeaudry <adrienne.beaudry@gmail.com>
2018-01-19 13:25:36 +0100

    Update comment justifying data format for column price in table Patterns

--- database/migrations/2018_01_04_075944_create_patterns_table.php
+++ database/migrations/2018_01_04_075944_create_patterns_table.php
@@ -19,9 +19,12 @@ class CreatePatternsTable extends Migration
             $table->unsignedInteger('company_id', false);
             $table->string('redirect_url');
             $table->string('company_pattern_id')->nullable();
-            // PRICE below
-            // change to decimal('price', 12, 4) when figure out how to remove price range in Named
-            // and dealing with different currencies
+            // PRICE:
+            // Ideally the format should be decimal('price', 12, 4)
+            // however price format for company Named is a price range with a dash (ex: "€12,00-€14,00")
+            //  -- plus the issue of currency -- which called for a string instead.
+            // In the future, I will convert the price data format to a decimal,
+            // as I develop further functionality such as advanced search, sorting and filtering on the page.
             $table->string('price')->nullable();
             $table->string('size_type', 100)->nullable();
             $table->tinyInteger('format', false, true)->nullable();
